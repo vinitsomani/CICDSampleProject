7d7649ebc311d671b7c1c71a764b4f5ad7fea0d1
manas <manas1.mishra@ril.com>
2018-11-01 13:14:38 +0530

    Cicd first push

---
+++ CICDSampleProject/CICDSampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICDSampleProject
+//
+//  Created by Manas Mishra on 01/11/18.
+//  Copyright © 2018 JioMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
